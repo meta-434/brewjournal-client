352e4cfc80c4b1eb136f845a261a4b12a68a6f06
Alex Hapgood <ach1@vt.edu>
2022-09-18 18:15:49 -0400

    Add Auth0 login redirect

--- src/App.tsx
+++ src/App.tsx
@@ -6,31 +6,38 @@ import Ratings from "./Components/Ratings";
 import Login from "./Components/Login";
 import Register from "./Components/Register";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <div>
-          {/* All this Navbar component does is provide links that the Routes component can match & serve */}
-          <Navbar />
-          {/* A <Routes> looks through its children <Route>s and
+    <Auth0Provider
+      domain={process.env.REACT_APP_DEV_AUTH0_DOMAIN || ""}
+      clientId={process.env.REACT_APP_DEV_AUTH0_CLIENT_ID || ""}
+      redirectUri={window.location.origin}
+    >
+      <div className="App">
+        <Router>
+          <div>
+            {/* All this Navbar component does is provide links that the Routes component can match & serve */}
+            <Navbar />
+            {/* A <Routes> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-          <Routes>
-            <Route path="/recipes" element={<Recipes />} />
+            <Routes>
+              <Route path="/recipes" element={<Recipes />} />
 
-            <Route path="/ratings" element={<Ratings />} />
+              <Route path="/ratings" element={<Ratings />} />
 
-            <Route path="/login" element={<Login />} />
+              <Route path="/login" element={<Login />} />
 
-            <Route path="/register" element={<Register />} />
+              <Route path="/register" element={<Register />} />
 
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </div>
-      </Router>
-    </div>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </div>
+        </Router>
+      </div>
+    </Auth0Provider>
   );
 }
 
--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,3 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const Auth0LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return <button onClick={() => loginWithRedirect()}>Log In! ☕️</button>;
+};
+
+const Auth0LogoutButton = () => {
+  const { logout } = useAuth0();
+
+  return (
+    <button onClick={() => logout({ returnTo: window.location.origin })}>
+      Log Out
+    </button>
+  );
+};
+
 export default function Login() {
-  return <div>Login!</div>;
+  const [count, setCount] = useState(0);
+
+  // useEffect(() => {
+  //   const id = setInterval(() => {
+  //     setCount(count + 1);
+  //   }, 1000);
+  //   return () => clearInterval(id);
+  // });
+
+  return (
+    <div>
+      <p>Login!</p>
+      <p>🗿{count}?</p>
+      <button onClick={() => setCount(count + 1)}>👍</button>
+      <button onClick={() => setCount(count - 1)}>👎</button>
+      <Auth0LoginButton />
+    </div>
+  );
 }
