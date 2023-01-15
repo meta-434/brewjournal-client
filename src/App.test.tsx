f1fd0e8c164f74d10290204835e77fe46d5142b5
Alex Hapgood <ach1@vt.edu>
2023-01-15 00:02:02 -0500

    wip on tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer";
+import App from "./App";
 
-test('renders learn react link', () => {
-    render(<App />);
+test("renders navbar", () => {
+    const app = renderer.create(<App />);
     const linkElement = screen.getByText(/learn react/i);
     expect(linkElement).toBeInTheDocument();
 });
